cd6a9002ead331c908e4b65946b72469049c25f7
Peter de Blanc <spaceandgames@gmail.com>
2014-10-17 13:12:51 +0900

    Remove logging from queue.js.

--- queue.js
+++ queue.js
@@ -65,26 +65,3 @@ PriorityQueue.prototype = {
         return this.heap[i].priority < this.heap[j].priority;
     }
 }
-
-///////////////////   Tests    /////////////////////////////
-
-var queue = new PriorityQueue();
-        
-queue.push({p:'two'}, 2);
-queue.push({p:'three'}, 3);
-queue.push({p:'five'}, 5);
-queue.push({p:'1st one'}, 1);
-queue.push({p:'zero'}, 0);
-queue.push({p:'nine'}, 9);
-queue.push({p:'2nd one'}, 1);
-        
-console.log(queue.heap.toString()); // => 0,1,1,3,2,9,5 
-
-console.log(queue.pop()); // => {p:'zero'}
-console.log(queue.pop()); // => {p:'1st one'}
-console.log(queue.heap.toString()); // => 1,2,9,3,5
-
-queue.push({p:'one-half'}, 0.5);
-console.log(queue.heap.toString()); // => 0.5,2,1,3,5,9
-
-// TODO: test negative numbers
