6551cb664d93ffb4a9153fde515483216980c41e
Peter de Blanc <spaceandgames@gmail.com>
2014-10-26 08:36:03 +0900

    Make a Universe class.

--- scripts/biomes.js
+++ scripts/biomes.js
@@ -1,42 +1,40 @@
-universe.biomes = {
-    grass: new Biome({
-        name: 'grass',
-        symbol: '草'
-    }),
-    tree: new Biome({
-        name: 'tree',
-        symbol: '木',
-        passable: false,
-        bias: -1
-    }),
-    woods: new Biome({
-        name: 'woods',
-        symbol: '林',
-        passable: true
-    }),
-    forest: new Biome({
-        name: 'forest',
-        symbol: '森',
-        passable: true
-    }),
-    water: new Biome({
-        name: 'water',
-        symbol: '水',
-        passable: false
-    }),
-    void: new Biome({
-        name: 'void',
-        symbol: '無',
-        passable: 0,
-        bias: -100
-    }),
-    empty: new Biome({
-        name: 'empty',
-        symbol: '無',
-        max_items: 1,
-        bias: -100 
-    })
-}
+universe.biome({
+    name: 'grass',
+    symbol: '草'
+})
+universe.biome({
+    name: 'tree',
+    symbol: '木',
+    passable: false,
+    bias: -1
+})
+universe.biome({
+    name: 'woods',
+    symbol: '林',
+    passable: true
+})
+universe.biome({
+    name: 'forest',
+    symbol: '森',
+    passable: true
+})
+universe.biome({
+    name: 'water',
+    symbol: '水',
+    passable: false
+})
+universe.biome({
+    name: 'void',
+    symbol: '無',
+    passable: 0,
+    bias: -100
+})
+universe.biome({
+    name: 'empty',
+    symbol: '無',
+    max_items: 1,
+    bias: -100 
+})
 
 universe.affinity.friends('grass', 'grass')
 universe.affinity.friends('water', 'water')
--- scripts/universe.js
+++ scripts/universe.js
@@ -1,13 +1,28 @@
-var universe = {
-    species: {
-        human: new Species({name: 'human', symbol: '人', lean_mass: 10}),
-        dragon: new Species({name: 'blue dragon', symbol: '龍', lean_mass: 200, vigor: 20})
-    },
-    products: {
-        katana: new Product({name: 'katana', symbol: '刀', action: 'toggle_wield', damage: function() { return {'cut': 5} }}),
-        longsword: new Product({name: 'longsword', symbol: '剣', action: 'toggle_wield', damage: function() { return {'cut': 5} }}),
-        meat: new Product({name: 'meat', symbol: '肉', fat: 2, action: 'eat'})
-    }
-}
+function Universe(attributes) {
+    this.specieses = {}
+    this.products = {}
+    this.biomes = {}
 
-console.log('universe', universe)
+    this.species = function(attributes) {
+        var species = new Species(attributes)
+        this.specieses[species.name] = species
+    }
+
+    this.product = function(attributes) {
+        var product = new Product(attributes)
+        this.products[product.name] = product
+    }
+
+    this.biome = function(attributes) {
+        var biome = new Biome(attributes)
+        this.biomes[biome.name] = biome
+    }
+} 
+
+var universe = new Universe()
+
+universe.species({name: 'human', symbol: '人', lean_mass: 10})
+universe.species({name: 'blue dragon', symbol: '龍', lean_mass: 200, vigor: 20})
+universe.product({name: 'katana', symbol: '刀', action: 'toggle_wield', damage: function() { return {'cut': 5} }})
+universe.product({name: 'longsword', symbol: '剣', action: 'toggle_wield', damage: function() { return {'cut': 5} }})
+universe.product({name: 'meat', symbol: '肉', fat: 2, action: 'eat'})
