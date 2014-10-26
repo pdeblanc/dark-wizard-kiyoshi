568d807fb48bd1d8b43f74295a76129a2a8b4b81
Peter de Blanc <spaceandgames@gmail.com>
2014-10-26 20:58:46 +0900

    Add more humans and swords.

--- scripts/kiyoshi.js
+++ scripts/kiyoshi.js
@@ -5,13 +5,14 @@ STATS = ['power', 'speed', 'vigor', 'lean_mass']
 initialize = function() {
     var plane = new WildernessPlane({width: 64, height: 64})
     var player = universe.clades.human.create()
-    var jimmy = universe.clades.human.create()
-    var dragon = universe.clades["blue dragon"].create()
     plane.place_randomly(player)
-    plane.place_randomly(jimmy)
-    plane.place_randomly(dragon)
-    plane.place_randomly(universe.products.katana.create())
-    plane.place_randomly(universe.products.longsword.create())
+    for (var i = 0; i < 30; i++)
+        plane.place_randomly(universe.clades.human.create())
+    plane.place_randomly(universe.clades["blue dragon"].create())
+    for (var i = 0; i < 2; i++) {
+        plane.place_randomly(universe.products.katana.create())
+        plane.place_randomly(universe.products.longsword.create())
+    }
     for (var i = 0; i < 100; i++) {
         plane.place_randomly(universe.products.meat.create())
     }
