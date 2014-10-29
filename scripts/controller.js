ad93c2f23c3fc10180fc1534403957f8e444a8a2
Peter de Blanc <spaceandgames@gmail.com>
2014-10-29 22:59:32 +0900

    Add some buttons for actions.

--- scripts/controller.js
+++ scripts/controller.js
@@ -39,9 +39,74 @@ function Controller(attributes) {
     }
     // tell player about controller
     this.being.controllers.push(this)
-    // set up display
-    this.being.viewports.push(new PlayerViewport({being: this.being, controller: this, container: this.container}))
-    new PlaneViewport({plane: this.being.inventory, controller: this})
+    // button creation method
+    this.button = function(f, label) {
+        var button = $('<button />').addClass('action')
+            .click(function() {
+                f.apply(controller)
+            })
+        for (var i = 0; i < label.length; i++) {
+            var c = label[i]
+            if (c == c.toUpperCase())
+                button.append($('<span />').text(c.toLowerCase()).addClass('key-label'))
+            else
+                button.append($('<span />').text(c))
+        }
+        return button
+    }
+    // button functions
+    this.cancel = function() {
+        this.cancel_partial_commands()
+    }
+    this.west = function() {
+        if (this.partial_command)
+            this.click(this.being.square.west())
+        else if (this.being.square.west().permit_entry(this.being))
+            this.push_command(['west'])
+        else
+            this.push_command(['attack', this.being.square.west()])
+        event.preventDefault()
+        return false;
+    }
+    this.east = function() {
+        if (this.partial_command)
+            this.click(this.being.square.east())
+        else if (this.being.square.east().permit_entry(this.being))
+            this.push_command(['east'])
+        else
+            this.push_command(['attack', this.being.square.east()])
+        event.preventDefault()
+        return false;
+    }
+    this.north = function() {
+        if (this.partial_command)
+            this.click(this.being.square.north())
+        else if (this.being.square.north().permit_entry(this.being))
+            this.push_command(['north'])
+        else
+            this.push_command(['attack', this.being.square.north()])
+        event.preventDefault()
+        return false;
+    }
+    this.south = function() {
+        if (this.partial_command)
+            this.click(this.being.square.south())
+        else if (this.being.square.south().permit_entry(this.being))
+            this.push_command(['south'])
+        else
+            this.push_command(['attack', this.being.square.south()])
+        event.preventDefault()
+        return false;
+    }
+    this.get = function() {
+        this.push_command(['get'])
+    }
+    this.look = function() {
+        this.set_partial_command(['look'])
+    }
+    this.rest = function() {
+        this.push_command(['rest'])
+    }
     // set up event listeners
     var controller = this
     document.body.addEventListener(
@@ -52,44 +117,16 @@ function Controller(attributes) {
                 controller.cancel_partial_commands()
             }
             if (event.keyCode == 37) {
-                if (controller.partial_command)
-                    controller.click(controller.being.square.west())
-                else if (controller.being.square.west().permit_entry(being))
-                    controller.push_command(['west'])
-                else
-                    controller.push_command(['attack', controller.being.square.west()])
-                event.preventDefault()
-                return false;
+                controller.west()
             }
             if (event.keyCode == 38) {
-                if (controller.partial_command)
-                    controller.click(controller.being.square.north())
-                else if (controller.being.square.north().permit_entry(being))
-                    controller.push_command(['north'])
-                else
-                    controller.push_command(['attack', controller.being.square.north()])
-                event.preventDefault()
-                return false;
+                controller.north()
             }
             if (event.keyCode == 39) {
-                if (controller.partial_command)
-                    controller.click(controller.being.square.east())
-                else if (controller.being.square.east().permit_entry(being))
-                    controller.push_command(['east'])
-                else
-                    controller.push_command(['attack', controller.being.square.east()])
-                event.preventDefault()
-                return false;
+                controller.east()
             }
             if (event.keyCode == 40) {
-                if (controller.partial_command)
-                    controller.click(controller.being.square.south())
-                else if (controller.being.square.south().permit_entry(being))
-                    controller.push_command(['south'])
-                else
-                    controller.push_command(['attack', controller.being.square.south()])
-                event.preventDefault()
-                return false;
+                controller.south()
             }
         },
         false
@@ -99,13 +136,23 @@ function Controller(attributes) {
         function(event) {
             var charStr = String.fromCharCode(event.which || event.keyCode)
             if (charStr == 'g')
-                controller.push_command(['get'])
+                this.get()
             if (charStr == 'l')
-                controller.set_partial_command(['look'])
+                this.look()
             if (charStr == 'p')
-                controller.push_command(['rest'])
+                this.rest()
         },
         false
     )
+    // set up buttons
+    $(this.container).append(
+        $('<div />').addClass('panel')
+        .append(this.button(this.rest, 'sleeP'))
+        .append(this.button(this.look, 'Look'))
+        .append(this.button(this.get, 'Get'))
+    )
+
+    // set up display
+    this.being.viewports.push(new PlayerViewport({being: this.being, controller: this, container: this.container}))
 }
 
