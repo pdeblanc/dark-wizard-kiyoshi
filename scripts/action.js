551416aea275749c80df526bec656c6cba9de030
Peter de Blanc <spaceandgames@gmail.com>
2014-11-02 23:38:25 +0900

    Move actions into action.js.

---
+++ scripts/action.js
@@ -0,0 +1,160 @@
+actions = {}
+
+function Action() {
+}
+
+// action.execute(subject, direct_object, indirect_object)
+// return true if action is successful
+// return false if action is unsuccessful
+// return a number 0 < x < 1 if the action is in progress; the same action will be repeated next turn (not implemented)
+Action.prototype.execute = function() {
+    return false
+}
+
+actions.north = new Action()
+actions.north.execute = function(subject) {
+    return subject.moveto(subject.square.north())
+}
+
+actions.south = new Action()
+actions.south.execute = function(subject) {
+    return subject.moveto(subject.square.south())
+}
+
+actions.east = new Action()
+actions.east.execute = function(subject) {
+    return subject.moveto(subject.square.east())
+}
+
+actions.west = new Action()
+actions.west.execute = function(subject) {
+    return subject.moveto(subject.square.west())
+}
+
+actions.wait = new Action()
+actions.wait.execute = function(subject) {
+    subject.tell("You wait.")
+    return true
+}
+
+actions.put = new Action()
+actions.put.execute = function(subject, item, square) {
+    var success = item.moveto(square)
+    if (success)
+        subject.tell("You put " + item.the() + " into " + square.the() + ".")
+    return success
+}
+
+actions.rest = new Action()
+actions.rest.execute = function(subject) {
+    if (subject.health >= 1) {
+        subject.tell("You are already fully healed!")
+        return false;
+    }
+    subject.tell('You rest...')
+    subject.health = Math.min(1, subject.health + 0.01)
+    subject.redraw()
+    return subject.health
+}
+
+actions.get = new Action()
+actions.get.execute = function(subject) {
+    var gotten_items, item, vacancy
+    gotten_items = []
+    while ((item = subject.square.items[0]) && (vacancy = subject.inventory.vacancy(item))) {
+        item.moveto(vacancy)
+        gotten_items.push(item)
+    }
+    if (gotten_items.length) {
+        subject.tell("You get " + english.list(gotten_items) + ".")
+        return true
+    }
+    if (subject.square.items[0])
+        subject.tell("You do not have space for " + subject.square.items[0].a() + ".")
+    else
+        subject.tell("There is nothing to get.")
+    return false
+}
+
+actions.eat = new Action()
+actions.eat.execute = function(subject, square) {
+    if (square.items.length == 0) {
+        subject.tell("There is nothing there to eat.")
+        return;
+    }
+    var item = square.items[0]
+    if (!(item.fat)) {
+        subject.tell(item.The() + " does not appear to be edible.")
+        return false;
+    }
+    subject.tell("You eat " + item.the() + ".")
+    subject.body_fat += item.fat
+    item.destroy()
+    return true;
+}
+
+actions.look = new Action()
+actions.look.execute = function(subject, square) {
+    var item_names = [square.a()]
+    for (var i = 0; i < square.beings.length; i++)
+        item_names.push(square.beings[i].a())
+    for (var i = 0; i < square.items.length; i++)
+        item_names.push(square.items[i].a())
+    subject.tell('You see ' + english.list(item_names) + '.')
+    return false
+}
+
+actions.attack = new Action()
+actions.attack.execute = function(subject, target_square) {
+    for (var i = 0; i < target_square.beings.length; i++) {
+        var being = target_square.beings[i]
+        if (subject.wielding) {
+            being.receive_damage(subject.wielding.attack, subject)
+        }
+        else {
+            var attack = subject.attacks[0]
+            being.receive_damage(attack, subject)
+        }
+        return true
+    }
+}
+
+actions.toggle_wield = new Action()
+actions.toggle_wield.execute = function(subject, square) {
+    if (square.items.length == 0) {
+        subject.tell("There is nothing there to wield.")
+        return;
+    }
+    var item = square.items[0]
+    if (subject.wielding == item)
+        return actions.unwield.execute(subject, square)
+    else
+        return actions.wield.execute(subject, square)
+}
+
+actions.wield = new Action()
+actions.wield.execute = function(subject, square) {
+    if (square.items.length == 0) {
+        subject.tell("There is nothing there to wield.")
+        return;
+    }
+    var item = square.items[0]
+    if (subject.wielding)
+        $(subject.wielding.span).removeClass('wielded')
+    subject.wielding = item
+    item.wielded_by = subject
+    item.span.className += ' wielded'
+    subject.tell('Now wielding ' + item.the() + '.')
+    return true
+}
+
+actions.unwield = new Action()
+actions.unwield.execute = function(subject, square) {
+    if (subject.wielding) {
+        $(subject.wielding.span).removeClass('wielded')
+        subject.wielding.wielded_by = false
+    }
+    subject.wielding = false
+    subject.tell('Now wielding nothing.')
+    return true
+}
--- scripts/being.js
+++ scripts/being.js
@@ -30,141 +30,6 @@ Being.prototype.power = 10
 Being.prototype.speed = 10
 Being.prototype.vigor = 10
 
-// actions
-Being.prototype.north = function() {
-    return this.moveto(this.square.north())
-}
-
-Being.prototype.south = function() {
-    return this.moveto(this.square.south())
-}
-
-Being.prototype.east = function() {
-    return this.moveto(this.square.east())
-}
-
-Being.prototype.west = function() {
-    return this.moveto(this.square.west())
-}
-
-Being.prototype.wait = function() {
-    this.tell("You wait.")
-    return true
-}
-
-Being.prototype.put = function(item, square) {
-    var success = item.moveto(square)
-    if (success)
-        this.tell("You put " + item.the() + " in " + square.the() + ".")
-    return success
-}
-
-Being.prototype.rest = function() {
-    if (this.health >= 1) {
-        this.tell("You are already fully healed!")
-        return false;
-    }
-    this.tell('You rest...')
-    this.health = Math.min(1, this.health + 0.01)
-    this.redraw()
-    return this.health
-}
-
-Being.prototype.get = function() {
-    var gotten_items, item, vacancy
-    gotten_items = []
-    while ((item = this.square.items[0]) && (vacancy = this.inventory.vacancy(item))) {
-        item.moveto(vacancy)
-        gotten_items.push(item)
-    }
-    if (gotten_items.length) {
-        this.tell("You get " + english.list(gotten_items) + ".")
-        return true
-    }
-    if (this.square.items[0])
-        this.tell("You do not have space for " + this.square.items[0].a() + ".")
-    else
-        this.tell("There is nothing to get.")
-    return false
-}
-
-Being.prototype.eat = function(square) {
-    if (square.items.length == 0) {
-        this.tell("There is nothing there to eat.")
-        return;
-    }
-    var item = square.items[0]
-    if (!(item.fat)) {
-        this.tell(item.The() + " does not appear to be edible.")
-        return false;
-    }
-    this.tell("You eat " + item.the() + ".")
-    this.body_fat += item.fat
-    item.destroy()
-    return true;
-}
-
-Being.prototype.look = function(square) {
-    var item_names = [square.a()]
-    for (var i = 0; i < square.beings.length; i++)
-        item_names.push(square.beings[i].a())
-    for (var i = 0; i < square.items.length; i++)
-        item_names.push(square.items[i].a())
-    this.tell('You see ' + english.list(item_names) + '.')
-    return false
-}
-
-Being.prototype.attack = function(target_square) {
-    for (var i = 0; i < target_square.beings.length; i++) {
-        var being = target_square.beings[i]
-        if (this.wielding) {
-            being.receive_damage(this.wielding.attack, this)
-        }
-        else {
-            var attack = this.attacks[0]
-            being.receive_damage(attack, this)
-        }
-        return true
-    }
-}
-
-Being.prototype.toggle_wield = function(square) {
-    if (square.items.length == 0) {
-        this.tell("There is nothing there to wield.")
-        return;
-    }
-    var item = square.items[0]
-    if (this.wielding == item)
-        return this.unwield(square)
-    else
-        return this.wield(square)
-}
-
-Being.prototype.wield = function(square) {
-    if (square.items.length == 0) {
-        this.tell("There is nothing there to wield.")
-        return;
-    }
-    var item = square.items[0]
-    if (this.wielding)
-        $(this.wielding.span).removeClass('wielded')
-    this.wielding = item
-    item.wielded_by = this
-    item.span.className += ' wielded'
-    this.tell('Now wielding ' + item.the() + '.')
-    return true
-}
-
-Being.prototype.unwield = function(square) {
-    if (this.wielding) {
-        $(this.wielding.span).removeClass('wielded')
-        this.wielding.wielded_by = false
-    }
-    this.wielding = false
-    this.tell('Now wielding nothing.')
-    return true
-}
-
 // methods
 Being.prototype.notify = function() {
     this.viewports.forEach(function(viewport) {viewport.render()})
@@ -175,7 +40,7 @@ Being.prototype.act = function(callback) {
     var obj = this
     if (this.controllers.length > 0) {
         this.controllers[0].set_callback(function(command) {
-            var success = obj[command[0]].apply(obj, command.slice(1))
+            var success = actions[command[0]].execute(obj, command[1], command[2])
             if (success) {
                 // lose a bit over one pound per day due to very active lifestyle
                 // remove leading 10000 when done testing
@@ -201,7 +66,7 @@ Being.prototype.act = function(callback) {
             if (squares[i] && squares[i].beings.length && this.hostile(squares[i].beings[0]))
                 command = ["attack", squares[i]]
         }
-        this[command[0]].apply(this, command.slice(1))
+        actions[command[0]].execute(obj, command[1], command[2])
         callback()
     }
 }
