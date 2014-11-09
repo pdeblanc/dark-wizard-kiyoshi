2f4da192c5b6f5a3cb543ddd1a7e558d14510894
Peter de Blanc <spaceandgames@gmail.com>
2014-11-09 22:26:49 +0900

    Give battleships iron corpses.

--- scripts/kiyoshi.js
+++ scripts/kiyoshi.js
@@ -9,11 +9,12 @@ Item
         .phylum({name: 'rapier', symbol: '剣', action: actions.toggle_wield, attack: {stab: 3}, level: 2})
     .kingdom({name: 'meat', symbol: '肉', fat: 1, action: actions.eat, attack: {slap: .5}})
     .kingdom({name: 'ash', symbol: '灰', bias: -10})
+    .kingdom({name: 'iron', symbol: '鉄', bias: -1})
     .kingdom({name: 'green tea', symbol: '茶', action: actions.drink, drinkable: true})
 
 Being
     .kingdom({name: 'animal', generic: true, bias: -5, corpse: universe.products.meat})
-        .phylum({name: 'human', symbol: '人', lean_mass: 100, attacks: [{punch: 1}], inventory: {width: 2, height: 9}, level: 2, playable: true, bias: -4, habitat: {town: 2}})
+        .phylum({name: 'human', symbol: '人', lean_mass: 100, attacks: [{punch: 1}], inventory: {width: 2, height: 9}, level: 2, playable: true, bias: -6, habitat: {town: 2}})
             .clazz({name: 'samurai', symbol: '侍', attacks: [{cut: 3}], playable: false})
         .phylum({name: 'blue dragon', symbol: '龍', lean_mass: 10000, vigor: 100, attacks: [{burn: 9}], can_fly: true, inventory: {width: 1, height: 3}, level: 10})
         .phylum({name: 'cat', symbol: '猫', lean_mass: 10, vigor: 3, attacks: [{scratch: 1}], playable: true})
@@ -25,7 +26,7 @@ Being
         .phylum({name: 'horse', symbol: '馬', lean_mass: 850, vigor: 29, speed: 13, attacks: [{kick: 2}], level: 2, habitat: {grass: 5}, bias: -9})
         .phylum({name: 'cow', symbol: '牛', lean_mass: 1000, vigor: 32, speed: 8, attacks: [{kick: 2}], level: 2, habitat: {grass: 5}, bias: -9})
         .phylum({name: 'fish', symbol: '魚', lean_mass: 2, vigor: 2, attacks: [{bite: 0.2}], can_walk: false, can_swim: true, playable: true})
-        .phylum({name: 'battleship', symbol: '艦', lean_mass: 20000000, vigor: 4472, attacks: [{shoot: 300}], can_walk: false, can_swim: true, level: 256, inventory: {width: 4, height: 9}})
+        .phylum({name: 'battleship', symbol: '艦', lean_mass: 20000000, vigor: 4472, attacks: [{shoot: 300}], can_walk: false, can_swim: true, level: 256, inventory: {width: 4, height: 9}, corpse: universe.products.iron})
         .phylum({name: 'fire being', symbol: '火', lean_mass: 100, speed: 15, attacks: [{burn: 3}], level: 4, corpse: universe.products.ash})
 
 Square
